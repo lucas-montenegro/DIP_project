0ffdff0a6b1820993762e776c346d30db9d1c8c0
lucas-montenegro <lucasmaassuncao@gmail.com>
2021-09-10 04:27:25 -0300

    Updating custom.py for collab and adding a new images

--- custom/custom.py
+++ custom/custom.py
@@ -5,25 +5,27 @@ import datetime
 import numpy as np
 import skimage.draw
 import cv2
-from mrcnn.visualize import display_instances
 import matplotlib.pyplot as plt
 
 # Root directory of the project
-ROOT_DIR = "C:/Users/lucas/ufal/6_periodo/DIP/projeto_2/Custom_MaskRCNN/"
+ROOT_DIR = "/content/DIP_project"
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.visualize import display_instances
 from mrcnn.config import Config
 from mrcnn import model as modellib, utils
 
+# Ignoring warnings
+import warnings
+warnings.filterwarnings('ignore')
+
 # Path to trained weights file
-COCO_WEIGHTS_PATH = './mask_rcnn_coco.h5'
+COCO_WEIGHTS_PATH = '/content/DIP_project/mask_rcnn_coco.h5'
 
 # Directory to save logs and model checkpoints, if not provided
 # through the command line argument --logs
-DEFAULT_LOGS_DIR = './logs'
-
-
+DEFAULT_LOGS_DIR = '/content/DIP_project/logs'
 
 class CustomConfig(Config):
     """Configuration for training on the custom  dataset.
@@ -34,13 +36,13 @@ class CustomConfig(Config):
 
     # We use a GPU with 12GB memory, which can fit two images.
     # Adjust down if you use a smaller GPU.
-    IMAGES_PER_GPU = 1
+    IMAGES_PER_GPU = 2
 
     # Number of classes (including background)
     NUM_CLASSES = 1 + 2  # Background + dog and cat
 
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 5
+    STEPS_PER_EPOCH = 50
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
@@ -65,7 +67,7 @@ class CustomDataset(utils.Dataset):
         dataset_dir = os.path.join(dataset_dir, subset)
 
         # We mostly care about the x and y coordinates of each region
-        annotations = json.load(open('./dataset/' + subset + '/annotations.json'))
+        annotations = json.load(open('/content/DIP_project/dataset/' + subset + '/annotations.json'))
         # print(annotations1)
         annotations = list(annotations.values())  # don't need the dict keys
 
@@ -149,12 +151,12 @@ def train(model):
     """Train the model."""
     # Training dataset.
     dataset_train = CustomDataset()
-    dataset_train.load_custom("./dataset/", "train")
+    dataset_train.load_custom("/content/DIP_project/dataset/", "train")
     dataset_train.prepare()
 
     # Validation dataset
     dataset_val = CustomDataset()
-    dataset_val.load_custom("./dataset/", "val")
+    dataset_val.load_custom("/content/DIP_project/dataset/", "val")
     dataset_val.prepare()
 
     # *** This training schedule is an example. Update to your needs ***
@@ -164,7 +166,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=20,
+                epochs=5,
                 layers='heads')
 
 
